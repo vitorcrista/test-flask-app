517b37d98b572bf2d96fb978ef251d0b9cce6c04
vitorcrista <vrafaelcrista@gmail.com>
2024-09-23 14:06:08 +0100

    code in html

--- app.py
+++ app.py
@@ -12,48 +12,54 @@ def fitbit():
 
     if not code or not state:
         return """
-    <!DOCTYPE html>
-<html>
-<head>
-    <title>Error 404</title>
-</head>
-<body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: Arial, sans-serif;">
-    <div style="text-align: center;">
-        <h1>Page Not Found</h1>
-        <p>The requested resource was not found.</p>
-    </div>
-</body>
-</html>"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Error 404</title>
+        </head>
+        <body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: Arial, sans-serif;">
+            <div style="text-align: center;">
+                <h1>Page Not Found</h1>
+                <p>The requested resource was not found.</p>
+            </div>
+        </body>
+        </html>
+        """
 
-    return """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Authorization Successful</title>
-</head>
-<body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: Arial, sans-serif;">
-    <div style="text-align: center;">
-        <h1>Authorization Successful!</h1>
-        <p>You can now close this window.</p>
-    </div>
-</body>
-</html>"""
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Authorization Successful</title> 
+        <meta name="code" content="{code}">
+
+    </head>
+    <body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: Arial, sans-serif;">
+        <div style="text-align: center;">
+            <h1>Authorization Successful!</h1>
+            <p>You can now close this window.</p>
+        </div>
+    </body>
+    </html>
+    """
 
 
 @app.errorhandler(404)
 def not_found(e):
-    return """<!DOCTYPE html>
-<html>
-<head>
-    <title>Error 404</title>
-</head>
-<body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: Arial, sans-serif;">
-    <div style="text-align: center;">
-        <h1>Page Not Found</h1>
-        <p>The requested resource was not found.</p>
-    </div>
-</body>
-</html>"""
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Error 404</title>
+    </head>
+    <body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: Arial, sans-serif;">
+        <div style="text-align: center;">
+            <h1>Page Not Found</h1>
+            <p>The requested resource was not found.</p>
+        </div>
+    </body>
+    </html>
+    """
 
 
 if __name__ == "__main__":
