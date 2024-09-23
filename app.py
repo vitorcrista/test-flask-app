88d10c6ab9933b52ee536aeca16a4753f7fcc3ad
vitorcrista <vrafaelcrista@gmail.com>
2024-09-23 12:35:35 +0100

    1

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/fitbit/callback", methods=["GET"])
 def fitbit():
-    return "<h1>Test Flask App</h1>"
+    code = request.args.get('code')
+    state = request.args.get('state')
+
+    #sending the code back to the main server
+    
+    if not code or not state:
+        return render_template('400.html'), 400
+    
+    return render_template('200.html'), 200
+
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
 
 
 if __name__ == "__main__":
