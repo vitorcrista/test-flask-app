04bcb8f6a548d670ca9319b19f4f25535e269fee
vitorcrista <vrafaelcrista@gmail.com>
2024-10-01 00:04:00 +0100

    change url

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def fitbit():
 
     try:
         # Call the Google web app's endpoint
-        response = requests.post("https://test-fast-app-v50-69627867300.europe-southwest1.run.app/start-oauth", json=payload)
+        response = requests.post("https://chat4elderly-service-v0-69627867300.europe-southwest1.run.app/start-oauth", json=payload)
 
         print(f"response.text = {response.text}")
 
